5a1aab7a3c5946dd960331e002874b269f72e557
moquette78 <giovanni.simon58@gmail.com>
2018-11-20 16:35:17 +0100

    Add files via upload

--- JAVASCRIPT/myScript.js
+++ JAVASCRIPT/myScript.js
@@ -1,3 +1,31 @@
 function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
+//   document.getElementById("demo").innerHTML = "Start enregistrer ";
+  // window.open("https://www.w3schools.com");
+  if (window.DeviceOrientationEvent) {
+  //  window.addEventListener("devicemotion", motion, false ); 
+  document.getElementById("demo").innerHTML = "Start enregistrer ";
+  window.addEventListener('devicemotion', function(event) {
+  
+  console.log(event.acceleration.x + ' m/s2');
+  document.getElementById("demoX").innerHTML = Math.trunc(event.accelerationIncludingGravity.x);
+  document.getElementById("demoY").innerHTML = Math.trunc(event.accelerationIncludingGravity.y);
+  document.getElementById("demoZ").innerHTML = Math.trunc(event.accelerationIncludingGravity.z);
+  
+    
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, Math.trunc(event.accelerationIncludingGravity.x)+100);
+  ctx.stroke();  
+    
+    
+  });
+
 }
+else {
+ document.getElementById("demo").innerHTML = "Register not possible ";
+}
+}
+
+  
+
