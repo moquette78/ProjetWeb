ddf64962cb70c9c395ff2b8c99b3df18c97f3537
moquette78 <giovanni.simon58@gmail.com>
2018-11-22 15:37:20 +0100

    Add files via upload

---
+++ HTML/myphpfile.php
@@ -0,0 +1,8 @@
+<form action="" method="POST">
+  <input type="text" value="default value, you can edit it" name="myfield">
+    <input type="text" value="default value, you can edit it" name="myfield">
+  <input type="submit" value = "post">
+</form>
+
+
+
