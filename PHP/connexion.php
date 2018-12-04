9bab3e0066c104dfce5d4c7ba65c367a0b43bf0a
gio <jaiyo.contact@gmail.com>
2018-12-04 20:10:32 +0100

    ok

---
+++ PHP/connexion.php
@@ -0,0 +1,21 @@
+<?php
+
+    $fichier = fopen('fichier.txt', 'r+'); //ouverture du fichier en lecture et ecriture
+    $valide=false;
+        while($ligne=fgets($fichier)){
+            $mots = explode(" ", $ligne);
+            if ($mots[0]== $_POST['pseudo']) {
+                if (trim($mots[1])==$_POST['mdp']) {
+                	echo "Success";
+                	$valide=true;
+                	break;
+                }
+            }
+        }
+
+        if (!$valide) {
+        	echo "Failed";
+        }
+        fclose($fichier);
+
+?>
---
+++ PHP/inscription.php
@@ -0,0 +1,18 @@
+<?php
+
+    $fichier = fopen('fichier.txt', 'r+'); //ouverture du fichier en lecture et ecriture
+    $pseudovalide=true;
+
+        while($ligne=fgets($fichier)){
+            $mots = explode(" ", $ligne);
+            if ($mots[0]== $_POST['pseudo']) {
+                echo "Failed"; 
+                $pseudovalide=false;
+            }
+        }
+        if($pseudovalide){
+            fputs($fichier, $_POST['pseudo']." ".$_POST['mdp']."\n");
+            echo "Success";
+        }
+        fclose($fichier);
+?>
