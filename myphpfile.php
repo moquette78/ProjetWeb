0f1791946002684ea17598d4ad7988e9fb14a714
moquette78 <giovanni.simon58@gmail.com>
2018-12-07 11:56:49 +0100

    Delete myphpfile.php

--- myphpfile.php
+++
@@ -1,8 +0,0 @@
-<form action="" method="POST">
-  <input type="text" value="default value, you can edit it" name="myfield">
-    <input type="text" value="default value, you can edit it" name="myfield">
-  <input type="submit" value = "post">
-</form>
-
-
-
