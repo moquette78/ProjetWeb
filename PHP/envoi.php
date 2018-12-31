033d99cbc835715a69ec95878af8ccbc8ce6f99a
moquette78 <giovanni.simon58@gmail.com>
2018-12-31 16:31:13 +0100

    ok

--- PHP/envoi.php
+++ PHP/envoi.php
@@ -6,8 +6,12 @@ $_SESSION['y']=$_POST['y'];
 $_SESSION['z']=$_POST['z'];
 $_SESSION['date']=date("d.m.y");
 // Read JSON file
-$json = file_get_contents('./student_data.json');
+$json = file_get_contents('../JSON/document.json');
 
 //Decode JSON
 $json_data = json_decode($json,true);
+//Print data
+print_r($json_data);
 ?>
+
+
