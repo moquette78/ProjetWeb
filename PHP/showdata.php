2cd49bfb2564ec8ad3e13e62027f5e4042708047
moquette78 <giovanni.simon58@gmail.com>
2019-01-04 16:52:48 +0100

    Just show dates with the matching keywords

--- PHP/showdata.php
+++ PHP/showdata.php
@@ -13,9 +13,20 @@
  $json =  file_get_contents("../JSON/document.json");
  $json = json_decode($json,true);
  
+
  
- foreach(array_keys($json["user"],$_POST['userData']) as $index)	
- 	echo " Your Keyword: \"" . $json["keyword"][$index] . "\" Date of recording: " . $json["date"][$index] . "<br>"; 
+ foreach(array_keys($json["user"],$_POST['userData']) as $index){	
+ 
+//Show just the records with the matching keyword
+if($json["keyword"][$index] === $_POST["keyword_data"]) 
+	echo " Your Keyword: \"" . $json["keyword"][$index] . "\" Date of recording: " . $json["date"][$index] . "<br>"; 
+	
+
+	//if($_POST["start_date"] < $_POST["fin_date"])
+	//TODO
+
+}
+
 ?>
 
 </body>
