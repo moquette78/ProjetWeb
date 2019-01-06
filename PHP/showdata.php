86ec609e88da0ef65343f36cb89c1905d0cd5667
moquette78 <giovanni.simon58@gmail.com>
2019-01-06 19:29:44 +0100

    Modifications

--- PHP/showdata.php
+++ PHP/showdata.php
@@ -3,23 +3,49 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="../CSS/website.css">
+
+
+<style>
+
+#return_button {float: right;}
+tab {
+    display: inline-block; 
+    margin-left: 20px; 
+}
+p {
+    display: inline;	
+}
+
+h5 {
+    display: inline;	
+}
+
+   
+</style>
 </head>
 
 <body>
 
+<h1 id="headline">Show my data</h1><hr>
+
+<form>
+  <input id="return_button" type="button" value="Retourner!" onclick="history.back()">
+</form>
 
 <?php
- echo "You choosed user: " . $_POST['userData'] . "<br><br>";
+ echo "<h1>You choosed user: " . $_POST['userData'] . "</h1>";
  $json =  file_get_contents("../JSON/document.json");
  $json = json_decode($json,true);
  
 $graph_index = 0;
  
+//Look for the user with the matching keyword 
+
  foreach(array_keys($json["user"],$_POST['userData']) as $index){	
  
 //Show just the records with the matching keyword
 if($json["keyword"][$index] === $_POST["keyword_data"]) 
-	echo " Your Keyword: \"" . $json["keyword"][$index] . "\" Date of recording: " . $json["date"][$index] . "<br>"; 
+	echo "<h1> Your Keyword: \"" . $json["keyword"][$index] . "\" Date of recording: " . $json["date"][$index] . "</h1><br>"; 
 	
 $graph_index = $index;
 
@@ -34,16 +60,17 @@ $zArray = $json["z"][$graph_index];
 
 
 
-<canvas id="myX" width="100" height="100" style="border:1px solid #d3d3d3;"></canvas>
-<canvas id="myY" width="100" height="100" style="border:1px solid #d3d3d3;"></canvas>
-<canvas id="myZ" width="100" height="100" style="border:1px solid #d3d3d3;"></canvas>
-
+<canvas id="myX" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
+<canvas id="myY" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
+<canvas id="myZ" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
+<img src="xyz.jpeg"  alt="Italian Trulli">
+<br><p>Vitesse moyenne X: <h5 id="xMoyenne">fd</h5></p>  <p><tab>Vitesse moyenne Y: <h5 id="yMoyenne"> fd</h5></p><p><tab>Vitesse moyenne Z:<h5 id="zMoyenne"> fd</h5></p>
 <script type="text/javascript">
 
 	var arrX = <?php echo json_encode ($xArray); ?>;
 	var arrY = <?php echo json_encode ($yArray); ?>;
 	var arrZ = <?php echo json_encode ($zArray); ?>;
-	var sizeofgraph = 100; //this value needs the same as the value from the html document	
+	var sizeofgraph = 250; //this value needs the same as the value from the html document	
 	
 	/*var str = "";
 	for(var i=0; i<arrX.length; i++){
@@ -62,11 +89,16 @@ function drawGraphZ() {
     ctx = c.getContext("2d");
     
     ctx.moveTo(0, sizeofgraph/2);
-    ctx.lineTo(100,sizeofgraph/2);
+    ctx.lineTo(sizeofgraph,sizeofgraph/2);
     ctx.moveTo(0, sizeofgraph/2);
 
-    for(var i = 0; i < arrZ.length; i++)
+    var moyenne = 0;
+    var i = 0;
+    for(; i < arrZ.length; i++){
   	ctx.lineTo(i, (sizeofgraph/2)- parseInt(arrZ[i],10));
+	moyenne += parseInt(arrZ[i],10);
+    }
+    document.getElementById("zMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + " m/s";
 
 
     ctx.lineWidth = 1;
@@ -80,12 +112,17 @@ function drawGraphY() {
     ctx = c.getContext("2d");
     
     ctx.moveTo(0,sizeofgraph/2);
-    ctx.lineTo(100,sizeofgraph/2);
+    ctx.lineTo(sizeofgraph,sizeofgraph/2);
     ctx.moveTo(0, sizeofgraph/2);
     
 
-    for(var i = 0; i < arrY.length; i++)
-  	ctx.lineTo(i, (sizeofgraph/2) - parseInt(arrY[i],10));
+    var moyenne = 0;
+    var i = 0;
+    for(; i < arrY.length; i++){
+  	ctx.lineTo(i, (sizeofgraph/2)- parseInt(arrY[i],10));
+	moyenne += parseInt(arrY[i],10);
+    }
+    document.getElementById("yMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + " m/s";
 
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#0000ff';
@@ -99,18 +136,23 @@ function drawGraphX() {
     ctx = c.getContext("2d");
     
     ctx.moveTo(0,sizeofgraph/2);
-    ctx.lineTo(100,sizeofgraph/2);
+    ctx.lineTo(sizeofgraph,sizeofgraph/2);
     ctx.moveTo(0, sizeofgraph/2);
-        
-    for(var i = 0; i < arrX.length; i++)
+    
+    var moyenne = 0;
+    var i = 0;
+    for(; i < arrX.length; i++){
   	ctx.lineTo(i, (sizeofgraph/2)- parseInt(arrX[i],10));
-
+	moyenne += parseInt(arrX[i],10);
+    }
+    document.getElementById("xMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + " m/s";
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#ff0000';
+    
     ctx.stroke(); 
+
   }
-
-
 </script>
+
 </body>
 </html> 
