cceb3f104f76817f5cc2152bd05d9f2823657347
moquette78 <giovanni.simon58@gmail.com>
2019-01-06 21:07:38 +0100

    Add files via upload

--- PHP/envoi.php
+++ PHP/envoi.php
@@ -4,6 +4,9 @@ $_SESSION['keyword']=$_POST['keyword'];
 $_SESSION['x']=$_POST['x'];
 $_SESSION['y']=$_POST['y'];
 $_SESSION['z']=$_POST['z'];
+$_SESSION['a']=$_POST[''];
+$_SESSION['b']=$_POST['b'];
+$_SESSION['c']=$_POST['c'];
 $_SESSION['date']=date("d.m.y");
 // Read JSON file
 $json = file_get_contents('../JSON/document.json');
@@ -24,6 +27,9 @@ $json_data['date'][]=date("Y-m-d");
 $json_data['x'][]=$_POST['x'];
 $json_data['y'][]=$_POST['y'];
 $json_data['z'][]=$_POST['z'];
+$json_data['a'][]=$_POST['a'];
+$json_data['b'][]=$_POST['b'];
+$json_data['c'][]=$_POST['c'];
 
 $jsonData = json_encode($json_data);
 print_r($json_data);
