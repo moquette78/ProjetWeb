85b236d57b49441d2586e2d707fb72a531eb20ae
moquette78 <giovanni.simon58@gmail.com>
2019-01-07 22:05:20 +0100

    Add files via upload

--- PHP/envoi.php
+++ PHP/envoi.php
@@ -1,5 +1,7 @@
 <?php 
 session_start();
+//on ajoute l'enregistrement de l'utilisateur en variable de session
+
 $_SESSION['keyword']=$_POST['keyword'];
 $_SESSION['x']=$_POST['x'];
 $_SESSION['y']=$_POST['y'];
@@ -8,12 +10,13 @@ $_SESSION['a']=$_POST[''];
 $_SESSION['b']=$_POST['b'];
 $_SESSION['c']=$_POST['c'];
 $_SESSION['date']=date("d.m.y");
-// Read JSON file
+// Lis le fichier JSON
 $json = file_get_contents('../JSON/document.json');
 
 //Decode JSON
 $json_data = json_decode($json,true);
 
+//Ajout des données dans le fichier JSON
 $json_data['user'][]=$_SESSION['pseudo'];
 $json_data['keyword'][]=$_POST['keyword'];
 $json_data['date'][]=date("Y-m-d");
@@ -26,5 +29,6 @@ $json_data['c'][]=$_POST['c'];
 
 $jsonData = json_encode($json_data);
 print_r($json_data);
+//Ecrit les données dans le fichier
 file_put_contents('../JSON/document.json', $jsonData);
 ?>
