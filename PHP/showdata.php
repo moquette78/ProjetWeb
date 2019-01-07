5bc06a5a98f316c9b7c12bf11b062642a3c366a4
moquette78 <giovanni.simon58@gmail.com>
2019-01-08 00:24:48 +0100

    Bugfixes

--- PHP/showdata.php
+++ PHP/showdata.php
@@ -52,15 +52,12 @@ canvas {
 </header>
 <h1 id="headline">Show my data</h1><hr>
 
-<form>
-  <input id="return_button" type="button" value="Retourner!" onclick="history.back()">
-</form>
 
 <?php
- echo "<h1>You choosed user: " . $_POST['userData'] . "</h1>";
+
  $json =  file_get_contents("../JSON/document.json");
  $json = json_decode($json,true);
- 
+  
 $graph_index = 0;
  
 //Look for the user with the matching keyword 
@@ -68,9 +65,11 @@ $graph_index = 0;
  foreach(array_keys($json["user"],$_POST['userData']) as $index){	
  
 //Show just the records with the matching keyword
-if($json["keyword"][$index] === $_POST["keyword_data"]) 
+if($json["keyword"][$index] === $_POST["keyword_data"]){ 
+	echo "<h1>You choosed user: \"" . $_POST['userData'] . "\"</h1>";
+	echo "<h1>Seconds: " . $json["seconds"][$index] . "</h1>";
 	echo "<h1> Your Keyword: \"" . $json["keyword"][$index] . "\" Date of recording: " . $json["date"][$index] . "</h1><br>"; 
-	
+	}
 $graph_index = $index;
 
 }
@@ -89,7 +88,7 @@ $cArray = $json["c"][$graph_index];
 <canvas id="myX" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
 <canvas id="myY" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
 <canvas id="myZ" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
-<img src="xyz.jpeg"  alt="Italian Trulli"><brb><brb>
+<img src="xyz.jpeg"  alt="Italian Trulli" width="350" height="250"><brb><brb>
 
 <br><p>Vitesse moyenne X: <h5 id="xMoyenne">fd</h5></p>  <p><tab>Vitesse moyenne Y: <h5 id="yMoyenne"> fd</h5></p><p><tab>Vitesse moyenne Z:<h5 id="zMoyenne"> fd</h5></p>
 </div>
@@ -98,7 +97,8 @@ $cArray = $json["c"][$graph_index];
 <canvas id="myA" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
 <canvas id="myB" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
 <canvas id="myC" width="250" height="250" style="border:1px solid #d3d3d3;"></canvas>
-<br><p>Vitesse moyenne A: <h5 id="aMoyenne">fd</h5></p>  <p><tab>Vitesse moyenne B: <h5 id="bMoyenne"> fd</h5></p><p><tab>Vitesse moyenne C:<h5 id="cMoyenne"> fd</h5></p>
+<img src="coordinates.jpeg"  width="350" height="250"alt="Italian Trulli"><brb><brb>
+<br><p>Inclinaison A(X): <h5 id="aMoyenne"></h5></p>  <p><tab><tab><tab>Inclinaison B(Y): <h5 id="bMoyenne"></h5></p><p><tab><tab><tab><tab>Inclinaison C(Z):<h5 id="cMoyenne"></h5></p>
 </div>
 <script type="text/javascript">
 
@@ -212,7 +212,7 @@ function drawGraphA() {
   	ctx.lineTo(i, (sizeofgraph/2)- parseInt(arrA[i],10));
 	moyenne += parseInt(arrA[i],10);
     }
-    document.getElementById("aMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + " m/s";
+    document.getElementById("aMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + "";
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#222222';
     
@@ -236,7 +236,7 @@ function drawGraphB() {
   	ctx.lineTo(i, (sizeofgraph/2)- parseInt(arrB[i],10));
 	moyenne += parseInt(arrB[i],10);
     }
-    document.getElementById("bMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + " m/s";
+    document.getElementById("bMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + "";
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#558050';
     
@@ -260,7 +260,7 @@ function drawGraphC() {
   	ctx.lineTo(i, (sizeofgraph/2)- parseInt(arrC[i],10));
 	moyenne += parseInt(arrC[i],10);
     }
-    document.getElementById("cMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + " m/s";
+    document.getElementById("cMoyenne").innerHTML = Number.parseFloat(moyenne/i).toFixed(2) + "";
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#908010';
     
